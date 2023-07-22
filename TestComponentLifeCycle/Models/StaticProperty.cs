c99eeb03cae9ba709b14c1e6a4c4cbdeb5896a14
farida.seyidzade <farida.seyidzade@gmail.com>
2023-07-22 15:53:59 +0200

    Добавьте файлы проекта.

---
+++ TestComponentLifeCycle/Models/StaticProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace TestComponentLifeCycle.Models
+{
+    public class StaticProperty
+    {
+        public static bool IfUserConnected { get; set; }
+        public static bool CheckedRemember { get; set; }
+        public static string UserNameInSessionCookies { get; set; }
+    }
+}
